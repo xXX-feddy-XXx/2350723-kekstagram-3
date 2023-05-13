522968cf04055104ae0560f7f2dd751303888450
xXX-feddy-XXx <spiridonov.f@yandex.ru>
2023-05-13 23:24:37 +0300

    module5-task1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-function getRandomInt (a, b) {
-  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function checkStringLenght(str, len) {
-  return str.length <= len;
-}
-
-let info = [];
-
-for(let i = 0; i < 25; i++){
-  photoInfo.push({
-    id: i+1,
-    url: 'photos/' + (i+1) + '.jpg',
-    description: 'ФОТО',
-    likes: getRandomInteger(15, 200),
-    comments: getRandomInteger(0, 200)
-  });
-}
+import { info } from './data.js'
 
 console.log(info[24]);
